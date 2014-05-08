f4c717de252c596b8c75f3c95898df1a2770dfeb
hevega <hevega@linuxmint.linuxmint-domain>
2014-05-07 20:09:29 -0600

    First commit

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,6 +6,18 @@ angular.module('HTML5Player.controllers', [])
   .controller('MyCtrl1', ['$scope', function($scope) {
 
   }])
+  .controller('MainPlayer', ['$scope', function($scope) {
+          
+
+  }])
+  .controller('PlayList', ['$scope', function($scope) {
+          
+
+  }])
+  .controller('ShowLyrics', ['$scope', function($scope) {
+          
+
+  }])
   .controller('MyCtrl2', ['$scope', function($scope) {
 
   }]);
